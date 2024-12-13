c01158ccd02f401a0dc4fbab5da07675861d6118
Thuyvan31803 <lethuyvan3182003@gmail.com>
2024-12-13 15:37:09 +0700

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,8 +7,8 @@ export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
         <div className='Todo'>
             <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
             <div>
-                <button type='submit' className='todo-btn' onClick={() => editTodo(task.id)}>Edit</button>
-                <button type='submit' className='todo-btn' onClick={() => deleteTodo(task.id)}>Delete</button>
+                <button type='submit' className='todo-btn1' onClick={() => editTodo(task.id)}>Edit</button>
+                <button type='submit' className='todo-btn2' onClick={() => deleteTodo(task.id)}>Delete</button>
             </div>
         </div>
     )
